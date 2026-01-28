d4c0c6cb7820235b56146e52ce891cd62ef63660
Angela <auramnar@syr.edu>
2026-01-28 12:54:21 -0500

    Enhance code comments in 1-3-3.py
    Added comments for clarity on variable usage and functionality.

--- 1-python/solutions/1-3-3.py
+++ 1-python/solutions/1-3-3.py
@@ -15,22 +15,22 @@ Suggested approach:
 
 '''
 
-
+# Import modules 
 import json
 import os # python utility for creating file paths
 
-folder_name = "1-python/data"
+folder_name = "1-python/data" # directory where the json file will be saves
 
 
 text = input("Enter names and grades: ")
-students = []
+students = [] # store data as a dictionary
 for student in text.split(","): # split on comma creating smaller strings
-    name, grade = student.strip().split() # tuple unpacking
+    name, grade = student.strip().split() # tuple unpacking. remove spaces and split by spaces
     grade = float(grade)
     students.append({"name": name, "grade": grade}) # create a dictionary and add to students list
 
-file_path = os.path.join(folder_name, "students.json")  
-
+file_path = os.path.join(folder_name, "students.json")  # is used instead of manually typing / so it works across operating systems.
+# "1-python/data/students.json"
 
 with open(file_path, "w") as file:
-    json.dump(students, file)
+    json.dump(students, file) # write to file in json format
