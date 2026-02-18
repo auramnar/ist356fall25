7980cf99f352bfd7c0265a5ce3f045ca1c098b7b
Angela <auramnar@syr.edu>
2026-02-18 13:24:47 -0500

    Refactor purchase data handling and customer display
    Refactored code to load and display purchases for all months, and merged customer data with purchases. Added functionality to select a month and display customers who did not make a purchase.

--- 3-data/solutions/3-3-2.py
+++ 3-data/solutions/3-3-2.py
@@ -3,6 +3,34 @@ import pandas as pd
 import requests
 import numpy as np
 
+# march_purchases = pd.read.csv(f"{base}/purchases-mar.csv" )
+
+
+'''
+base = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/minimart/"
+customers = pd.read_csv(f"{base}/customers.csv")
+st.dataframe(customers)
+march_purchases = pd.read_csv(f"{base}/purchases-mar.csv")
+st.dataframe(march_purchases)
+
+
+months = ['jan', 'feb', 'mar', 'apr']
+months_dfs = []
+for month in months:
+    month_df = pd.read_csv(f"{base}/purchases-{month}.csv")
+    month_df['month'] = month # add lineage to the dataframe (e.g. add the month name)
+    months_dfs.append(month_df) # add the dataframe to a list of months     
+all_purchases = pd.concat(months_dfs) # concat the list of months together one dataframe
+st.dataframe(all_purchases) # print dataframe  
+combined = pd.merge(customers, all_purchases, on='customer_id', how='left') # merge customers with purchases
+st.dataframe(combined) # print combined dataframe
+
+select_month = st.selectbox("Select a month", options=['jan', 'feb', 'mar', 'apr'])
+cols_to_show = ['customer_id', 'firstname', 'lastname']
+did_not_buy = combined["order_id"].isnull()
+customers_who_did_not_buy = combined[did_not_buy][cols_to_show]
+
+'''
 
 base = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/minimart/"
 months = ['jan', 'feb', 'mar', 'apr']
@@ -20,4 +48,5 @@ st.dataframe(combined)
 cols= ['customer_id', 'firstname', 'lastname']
 did_not_buy = combined["order_id"].isnull()
 customers_who_did_not_buy = combined[did_not_buy][cols]
+
 st.dataframe(customers_who_did_not_buy, hide_index=True)
