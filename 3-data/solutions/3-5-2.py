61c34cc4449c355eab91830d882e70f816bf5107
Angela <auramnar@syr.edu>
2026-02-25 06:52:39 -0500

    Refactor Streamlit pivot table implementation

--- 3-data/solutions/3-5-2.py
+++ 3-data/solutions/3-5-2.py
@@ -10,12 +10,29 @@ Let's build an interactive pivot table in streamlit!
 - build the pivot table dataframe from the inputs. use the average for the `aggfunc`
 - display the pivot table!
 
+url = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/exam-scores/exam-scores.csv"
+exams = pd.read_csv(url)
+st.write ...
+st.dataframe ...
+
+cols = ['Class_Section', 'Exam_Version', 'Made_Own_Study_Guide', 'Did_Exam_Prep Assignment', 'Studied_In_Groups','Letter_Grade']
+measures = ['Completion_Time','Student_Score']
+
+col = st.selectbox('Select a column', cols) # column selection
+row_select = st.selectbox('Select a row', cols) # row selection
+if row_select in col:
+  col.remove(row_select)
+value = st.selectbox('Display the Average', measures)
+
+pivot_df = exams.pivot_table(index=row, columns=col, values=value, aggfunc='mean')
+
+st.dataframe(pivot_df)
 '''
 
-import numpy as np
+
 import pandas as pd
 import streamlit as st
-import pandas as pd
+
 
 url = "https://raw.githubusercontent.com/mafudge/datasets/refs/heads/master/exam-scores/exam-scores.csv"
 
@@ -32,4 +49,5 @@ col = st.selectbox('Select a column', cols) # column selection
 value = st.selectbox('Display the Average', measures)
 
 pivot_df = exams.pivot_table(index=row, columns=col, values=value, aggfunc='mean')
+
 st.dataframe(pivot_df)
