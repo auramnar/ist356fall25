202d2e23c49d739f94564478e7f23bf62ac22495
Angela <auramnar@syr.edu>
2026-03-30 08:20:13 -0400

    Refactor AI response generation and display result
    Updated API key placeholder and added result display.

--- 4-api/solutions/4-3-1.py
+++ 4-api/solutions/4-3-1.py
@@ -2,7 +2,7 @@
 curl -X 'POST' \
   'https://cent.ischool-iot.net/api/genai/generate?model=llama3%3Alatest&temperature=0.7&max_tokens=1000' \
   -H 'accept: application/json' \
-  -H 'X-API-KEY: ec25dc1e1297cfba51838bd3' \
+  -H 'X-API-KEY: "your api" \
   -H 'Content-Type: application/x-www-form-urlencoded' \
   -d 'query=what%20is%20the%20latest%20temp'
 
@@ -19,7 +19,7 @@ def generate_ai_response(prompt: str) -> str:
     
     url = "https://cent.ischool-iot.net/api/genai/generate"
     data = {'query': prompt}
-    headers={'X-API-KEY': 'ec25dc1e1297cfba51838bd3'}
+    headers={'X-API-KEY': 'your api'}
     response = requests.post(url, headers=headers, data=data)   
     response.raise_for_status()
     return response.json()
