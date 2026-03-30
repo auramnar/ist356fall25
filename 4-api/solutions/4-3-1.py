92b9627c17be70fa01dd562c35601a7974f70481
Angela <auramnar@syr.edu>
2026-03-30 09:02:59 -0400

    Implement spellcheck feature and update AI response method
    Added spellcheck functionality and modified AI response generation.

--- 4-api/solutions/4-3-1.py
+++ 4-api/solutions/4-3-1.py
@@ -11,20 +11,40 @@ curl -X 'POST' \
 import requests
 import streamlit as st
 
+#WRAP THE OPEN AI IN A FUNCTION
 def generate_ai_response(prompt: str) -> str:
     '''
     Generate AI response from the given prompt using the GenAI API. 
     
     '''
-    
+    #params = {'temperature': 0}
     url = "https://cent.ischool-iot.net/api/genai/generate"
     data = {'query': prompt}
     headers={'X-API-KEY': 'your api'}
-    response = requests.post(url, headers=headers, data=data)   
+    response = requests.post(url, headers=headers, data=data, params = params)   
     response.raise_for_status()
     return response.json()
 
+#check = generate_ai_response("Are you smart")
+#st.write(check)
+# FOR EACH PROMPT YOU ENGINEER MAKE A FUNCTION
+# SO YOU CAN CREATE DIFFERNT VERSIONS AND EVALUATE
+def spellcheck(text):
+  prompt = "Spell check the following text"
+  prompt += text "\n"
+  prompt += "for each misspelling provide one suggestion"
+  #prompt += "return as a list of dictionaries in JSON format"
+  check = generate_ai_response(prompt)
+  return check
 
+#MAIN PROGRAM
+st.title("SPELL CHECKER")
+text = st.text_area("Enter text")
+s_check = spellcheck(text)
+st.write(s_check)
+
+
+  
 st.title("FirstGPT")
 
 text = st.text_input("Enter your prompt here:")
