6c649da5a8b2a149b953098753b6a7b49b070f12
Angela <auramnar@syr.edu>
2026-04-08 12:41:25 -0400

    Refactor web scraping and output handling
    Updated the scraping logic to convert page content into a file-like object before parsing with pandas. Added print statement for the DataFrame instead of saving to CSV.

--- 5-web/5-2tables2.py
+++ 5-web/5-2tables2.py
@@ -1,5 +1,7 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
 import pandas as pd
+from io import StringIO
+
 
 def run(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False)
@@ -7,13 +9,18 @@ def run(playwright: Playwright) -> None:
     page = context.new_page()
     page.goto("https://en.wikipedia.org/wiki/National_Football_League")
 
+    # wait for tables to load
+    page.wait_for_selector("table")
+    
     # Let's scrape the page!
     # use pandas read_html to parse the HTML
-    dfs = pd.read_html(page.content())
+    html = StringIO(page.content()) #converts string into file-like object
+    dfs = pd.read_html(html)
 
     # print the table
     df_teams = dfs[2]
-    df_teams.to_csv('nfl_teams.csv', index = False)
+    print(df_teams)
+    #df_teams.to_csv('nfl_teams.csv', index = False)
 
     # ---------------------
     context.close()
